bc54905c50bc7af1b70253a8f0340584453d6934
ryon777 <85278026+ryon777@users.noreply.github.com>
2021-08-04 13:56:27 +0900

    Update register.php

--- register.php
+++ register.php
@@ -17,31 +17,30 @@
             <div class="form_contents">
             <h2>会員登録</h2>
                 <div class="hissu">必須</div><label>氏名</label><br>
-                <input type="text"class="formbox"size="40"name="name" required>
-                </div>
+                    <input type="text"class="formbox"size="40"name="name" required>
                 <div class="mail">
-                <div class="hissu">必須</div><label>メールアドレス</label><br>
-                <input type="text"class="formbox"size="40"name="mail" pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" required>
+                    <div class="hissu">必須</div><label>メールアドレス</label><br>
+                    <input type="text"class="formbox"size="40"name="mail" pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" required>
                 </div>
                 <div class="password">
-                <div class="hissu">必須</div><label>パスワード（半角英数字6文字以上）</label><br>
-                <input type="password"class="formbox"size="40"name="password"id="password"pattern="^[a-zA-Z0-9]{6,}$" required>
+                    <div class="hissu">必須</div><label>パスワード（半角英数字6文字以上）</label><br>
+                    <input type="password"class="formbox"size="40"name="password"id="password"pattern="^[a-zA-Z0-9]{6,}$" required>
                 </div>
                 <div class="password">
-                <div class="hissu">必須</div><label>パスワード確認</label><br>
-                <input type="password"class="formbox"size="40"name="confirm_password"id="confirm"oninput="ConfirmPassword(this)" required>
+                    <div class="hissu">必須</div><label>パスワード確認</label><br>
+                    <input type="password"class="formbox"size="40"name="confirm_password"id="confirm"oninput="ConfirmPassword(this)" required>
                 </div>
                 <div class="picture">
-                <label>プロフィール写真</label><br>
-                <input type="hidden"name="max_file_size"value="1000000"/>
-                <input type="file"size="40"name="picture">
+                    <label>プロフィール写真</label><br>
+                    <input type="hidden"name="max_file_size"value="1000000"/>
+                    <input type="file"size="40"name="picture">
                 </div>
                 <div class="comments">
-                <label>コメント</label><br>
-                <textarea rows="5"cols="45"name="comments"></textarea>
+                    <label>コメント</label><br>
+                    <textarea rows="5"cols="45"name="comments"></textarea>
                 </div>
                 <div class="toroku">
-                <input type="submit"class="submit_button"size="35"value="登録する">
+                    <input type="submit"class="submit_button"size="35"value="登録する">
                 </div>
             </div>
         </form>
